f6987fbb2ddc741df479b0ec766c8f748b01f5f1
unknown <alessio.fabiani@geo-solutions.it>
2014-02-19 12:54:16 +0100

    - changeMatrix package renaming

--- src/extension/wps-soil-sealing/wps-changematrix/src/main/java/org/geoserver/wps/gs/soilsealing/ChangeMatrixDTO.java
+++ src/extension/wps-soil-sealing/wps-changematrix/src/main/java/org/geoserver/wps/gs/soilsealing/ChangeMatrixDTO.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
-package org.geoserver.extension;
+package org.geoserver.wps.gs.soilsealing;
 
 import it.geosolutions.jaiext.changematrix.ChangeMatrixDescriptor.ChangeMatrix;
 
--- src/extension/wps-soil-sealing/wps-changematrix/src/main/java/org/geoserver/wps/gs/soilsealing/ChangeMatrixDeleteTransactionListener.java
+++ src/extension/wps-soil-sealing/wps-changematrix/src/main/java/org/geoserver/wps/gs/soilsealing/ChangeMatrixDeleteTransactionListener.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
-package org.geoserver.extension;
+package org.geoserver.wps.gs.soilsealing;
 
 import java.io.File;
 import java.util.ArrayList;
